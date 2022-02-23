31556b9302825fbc2da6980e5d339c7ff18ea5f5
Marcus M <78941703+GAGARR@users.noreply.github.com>
2022-02-23 18:33:55 +1000

    update: woah an actually structured website
    woah an actually structured website

--- main.js
+++ main.js
@@ -1,8 +1,2 @@
-$(document).ready(function() {
- 
-    setTimeout(function(){
-        $('body').addClass('loaded');
-        $('h1').css('color','#222222');
-    }, 3000);
- 
-});
+$(elem).css('z-index','0');
+setTimeout(function(){ $(elem).css('z-index','2'); },2000)
